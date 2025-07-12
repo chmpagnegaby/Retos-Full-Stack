cf6276648cfae0f7b4063fb8e062ef5790d8c41c
chmpagnegaby <hcgabriel7@gmail.com>
2025-07-12 02:00:16 +0200

    Segundo commit de prueba con actualizacion en el readme

---
+++ SEMANA 1/HelloWorlsd.js
@@ -0,0 +1,16 @@
+//Ejercicio primero, HelloWorld
+x = "Hello world";
+console.log(x);
+
+//Tipicos ejecicios de consola
+a  = 1;
+b = 2;
+
+console.log(a+b)
+
+//Variables
+let nombre = "Maria";
+const edad = 30;
+var ciudad = "Barcelona";
+
+console.log("Hola, soy " + nombre + " tengo " + edad + " años y vivo en " + ciudad + "." )
